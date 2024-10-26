bb20deed18959d8052ef273d42aad78fecbe7850
ettorebonfiglio <147381601+ettorebonfiglio@users.noreply.github.com>
2024-10-26 15:36:30 +0200

    s

--- es/calcolatrice.js
+++ es/calcolatrice.js
@@ -1,13 +1,16 @@
 let operazione="";
+let n1=0;
+let n2=0;
+let risultato=0;
 
 function somma(p1, p2) {
     return Number(p1) + Number(p2);
 }
 function sottrazione(p1, p2) {
-    return Number(p1) - Number(p2);
+  return Number(p1) - Number(p2);
 }
 function moltiplicazione(p1, p2) {
-    return Number(p1) * Number(p2);
+  return Number(p1) * Number(p2);
 }
 function divisione(p1, p2) {
     if(p2!=0)
@@ -16,52 +19,56 @@ function divisione(p1, p2) {
         return 0;
 }
 
-function lettura() {
-    let n1=document.getElementById("numero1");
-    let n2=document.getElementById("numero2");
-    let ris=document.getElementById("ris");
+function getNumVal(){
+    let n = document.getElementById("numero").value;
+    document.getElementById("numero").value = "";
+    if(n==null || n==""){}
+    return n;
+}
 
-    console.log(n1.value);
-    console.log(n2.value);
+function scrittura() {
+    let ris=document.getElementById("ris");
+    ris.innerHTML=n1+' '+operazione+' '+n2+' = '+risultato;
+}
+
+function lettura() {
+    n2=getNumVal();
+    
+
+    console.log(n1);
+    console.log(n2);
     console.log(operazione);
 
-    if(n1.value==""){
+    if(n1==""){
         ris.innerHTML="SELEZIONA UN VALORE PER N1";
         return ;
     }
-    if(n2.value==""){
+    if(n2==""){
         ris.innerHTML="SELEZIONA UN VALORE N2";
         return ;
     }
-    if(operazione==""){
-        ris.innerHTML="SELEZIONA UN'OPERAZIONE";
-        return ;
-    }
-    if(operazione=="/" && n2.value==0){
-        ris.innerHTML="LA DIVISIONE E' IMPOSSIBILE";
-        return ;
-    }
+    
+    risultato=0;
 
-    let risultato=0;
     switch (operazione){
       case '+':
-          risultato=somma(n1.value,n2.value);
+          risultato=somma(n1,n2);
           break;
       case '-':
-          risultato=sottrazione(n1.value,n2.value);
+          risultato=sottrazione(n1,n2);
           break;
       case '*':
-          risultato=moltiplicazione(n1.value,n2.value);
+          risultato=moltiplicazione(n1,n2);
           break;
       case '/':
-          risultato=divisione(n1.value,n2.value);
+          risultato=divisione(n1,n2);
           break;
       }
-      ris.innerHTML=n1.value+' '+operazione+' '+n2.value+' = '+risultato;
-
+      scrittura();
 }
 
 function setOperazione(op, id){
+    n1=getNumVal();
     let container = document.getElementById(id);
     document.getElementById('somma').classList.remove('scelta');
     document.getElementById('sottrazione').classList.remove('scelta');
@@ -69,4 +76,5 @@ function setOperazione(op, id){
     document.getElementById('divisione').classList.remove('scelta');
     container.classList.add('scelta');
     operazione=op;
+    scrittura();
 }
