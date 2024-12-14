916248d2d6552baac7e87ab3c65f6d6a7ffc6b24
ettorebonfiglio <147381601+ettorebonfiglio@users.noreply.github.com>
2024-12-14 15:31:40 +0100

    ad

--- es/tabMigranti.js
+++ es/tabMigranti.js
@@ -1,11 +1,39 @@
 function apri(input){
-    let file = input.files[0];
+  let file = input.files[0];
 
-      let reader = new FileReader();
+  let reader = new FileReader();
 
-      reader.readAsText(file);
+  reader.readAsText(file);
 
-      reader.onload = function() {
-        document.getElementById("contenuto").innerHTML = reader.result;
-      }
+  reader.onload = function() {
+      document.getElementById("contenuto").innerHTML = costruisciTabella(reader.result);
+  }
+}
+
+function costruisciTabella(csv) {
+  console.log(csv);
+  
+  let righe = csv.split("\n");
+
+  let html = '<table class="table table-bordered table-striped mt-3">';
+  html += "<thead>";
+  html += "<tr>";
+  html += "<th> Anno </th>";
+  html += "<th> Numero </th>";
+  html += "</tr>";
+  html += "</thead>";
+  html += "<tbody>";
+  for (let i = 1; i < righe.length; i++) {
+      let colonne = righe[i].split(",");
+
+      html += "<tr>";
+      html += "<td>" + colonne[0] + "</td>";
+      html += "<td>" + colonne[1] + "</td>";
+      html += "</tr>";
+
+  }
+  html += "</tbody>";
+  html += '</table>';
+
+  return html;
 }
