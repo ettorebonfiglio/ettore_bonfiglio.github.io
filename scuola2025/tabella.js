765429c84a5fe3ead527cea5c85b4b86b968e801
ettorebonfiglio <147381601+ettorebonfiglio@users.noreply.github.com>
2025-02-05 10:46:51 +0100

    tabella

--- scuola2025/tabella.js
+++ scuola2025/tabella.js
@@ -1,5 +1,18 @@
 document.getElementById('csvFileInput').addEventListener('change', handleFileSelect, false);
 
+function apri(input){
+    let file = input.files[0];
+  
+    let reader = new FileReader();
+  
+    reader.readAsText(file);
+  
+    reader.onload = function() {
+        document.getElementById("contenuto").innerHTML = costruisciTabella(reader.result);
+    }
+  }
+  
+
 function handleFileSelect(event) {
     const file = event.target.files[0];
     const reader = new FileReader();
